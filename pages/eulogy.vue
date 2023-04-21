9668461a4bef3032119528f358c65b69c55f2b48
Eugene Musebe <eugenemusebe@Eugenes-MacBook-Pro-2.local>
2023-04-21 15:43:27 +0300

    upgraded yarn project

--- pages/eulogy.vue
+++ pages/eulogy.vue
@@ -8,7 +8,6 @@
       <h1 class="title">Eulogy</h1>
       <p> Content Extracted From The Eulogy</p>
       <p class="highlight">A Bit of important text that you want to stand out, such as any dressing requirements.</p>
-      <h2>Church Service <span>10:30am</span></h2>
       
       <!-- CONTENT END -->
     </div>
