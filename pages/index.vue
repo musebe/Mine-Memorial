2e8875e8314e55faf6f744b596cfbe499fdf039c
Eugene Musebe <eugenemusebe@Eugenes-MacBook-Pro-2.local>
2023-04-24 18:34:28 +0300

    links added

--- pages/index.vue
+++ pages/index.vue
@@ -6,8 +6,16 @@
     <div class="section tribute">
       <!-- CONTENT START -->
       <h1 class="title">In Loving Memory</h1>
+
+      <p>
+      For Google Maps Directions Use the following Link : <br/><br/>  <a href="https://goo.gl/maps/F2ytRutJVrPzuTmHA" target="_blank">Click Here For Directions To The Funeral</a>
+      </p>
       <img height="292" src="~/assets/frontimage.jpeg" alt="In loving memory" />
       <div class="subtitle"><div class="name">{{ $config.name }}</div>{{ $config.dates }}</div>
+
+      <p>
+      Download PDF Version of the Eulogy : <br/><br/>  <a href="https://res.cloudinary.com/hackit-africa/image/upload/v1682346749/elizabeth_wamitu_programme_1_1.pdf" target="_blank">Click Here To Download</a>
+      </p>
       <!-- CONTENT END -->
     </div>
   </div>
