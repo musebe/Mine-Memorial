4df74a0d3e2bdc4715d076392b3752ad71535953
Eugene Musebe <eugenemusebe@Eugenes-MacBook-Pro-2.local>
2023-04-24 17:00:34 +0300

    Added Maps

--- components/Nav.vue
+++ components/Nav.vue
@@ -12,7 +12,7 @@
 
         <li><NuxtLink to="/order-of-service"><span class="icon">
           <svg class="svg-inline--fa fa-map fa-w-18 fa-fw" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="map" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" data-fa-i2svg=""><path fill="currentColor" d="M0 117.66v346.32c0 11.32 11.43 19.06 21.94 14.86L160 416V32L20.12 87.95A32.006 32.006 0 0 0 0 117.66zM192 416l192 64V96L192 32v384zM554.06 33.16L416 96v384l139.88-55.95A31.996 31.996 0 0 0 576 394.34V48.02c0-11.32-11.43-19.06-21.94-14.86z"></path></svg>
-          </span><span class="menu"><span>Order of </span>Service</span></NuxtLink>
+          </span><span class="menu"><span></span>Service</span></NuxtLink>
           
           </li>
 
@@ -32,11 +32,11 @@
           </span><span class="menu">Gallery</span></NuxtLink></li>
         <li><NuxtLink to="/charity-donation"><span class="icon">
           <svg class="svg-inline--fa fa-hands-helping fa-w-20 fa-fw" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="hands-helping" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" data-fa-i2svg=""><path fill="currentColor" d="M488 192H336v56c0 39.7-32.3 72-72 72s-72-32.3-72-72V126.4l-64.9 39C107.8 176.9 96 197.8 96 220.2v47.3l-80 46.2C.7 322.5-4.6 342.1 4.3 357.4l80 138.6c8.8 15.3 28.4 20.5 43.7 11.7L231.4 448H368c35.3 0 64-28.7 64-64h16c17.7 0 32-14.3 32-32v-64h8c13.3 0 24-10.7 24-24v-48c0-13.3-10.7-24-24-24zm147.7-37.4L555.7 16C546.9.7 527.3-4.5 512 4.3L408.6 64H306.4c-12 0-23.7 3.4-33.9 9.7L239 94.6c-9.4 5.8-15 16.1-15 27.1V248c0 22.1 17.9 40 40 40s40-17.9 40-40v-88h184c30.9 0 56 25.1 56 56v28.5l80-46.2c15.3-8.9 20.5-28.4 11.7-43.7z"></path></svg>
-          </span><span class="menu"><span>Charity </span>Donations</span></NuxtLink></li>
+          </span><span class="menu"><span></span>Support</span></NuxtLink></li>
 
            <li><NuxtLink to="/maps"><span class="icon">
           <svg viewBox="0 0 192 192" xmlns="http://www.w3.org/2000/svg" fill="none"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path stroke="#f41b8ed9" stroke-width="12" d="M96 22a51.88 51.88 0 0 0-36.77 15.303A52.368 52.368 0 0 0 44 74.246c0 16.596 4.296 28.669 20.811 48.898a163.733 163.733 0 0 1 20.053 28.38C90.852 163.721 90.146 172 96 172c5.854 0 5.148-8.279 11.136-20.476a163.723 163.723 0 0 1 20.053-28.38C143.704 102.915 148 90.841 148 74.246a52.37 52.37 0 0 0-15.23-36.943A51.88 51.88 0 0 0 96 22Z"></path><circle cx="96" cy="74" r="20" stroke="#f41b8ed9" stroke-width="12"></circle></g></svg>
-          </span><span class="menu"><span>Burial</span> Location</span></NuxtLink>
+          </span><span class="menu"><span></span> Location</span></NuxtLink>
           
           </li>
       </ul>
